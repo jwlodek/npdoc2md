6f31a2979c461d24c6d0a7b91d50c4dae6250c9c
Jakub Wlodek <jwlodek.dev@gmail.com>
2020-03-18 09:57:59 -0400

    Ran code2npdoc on itself, generated docstrings

--- code2npdoc.py
+++ code2npdoc.py
@@ -30,23 +30,70 @@ __url__         = 'https://github.com/jwlodek/npdoc2md'
 
 
 class DocStringAttribute:
+    """Class representing a docstring attr table  
+
+    Attributes
+    ----------
+    attribute_name : str
+        Name of the attribute
+    attribute_elements : List[str]
+        list of attribute elements
+    """
+
 
     def __init__(self, attribute_name : str, elements : List[str]):
+        """Initializer for docstring attribute
+        """
+
         self.attribute_name = attribute_name
         self.attribute_elements = elements
 
+
 class GenerationInstance:
+    """Class representing a function/class/method
+
+    Attributes
+    ----------
+    name : str
+        Name of the instance
+    descriptors : DocStringAttribute
+        Descriptors for instance (Returns, Parameters, etc.)
+    doc_level : int
+        required tabs
+    """
+
 
     def __init__(self, name : str, doc_level : int):
+        """Initializer for GenerationInstance
+        """
+
         self.name = name
         self.descriptors = []
         self.doc_level = doc_level
 
 
     def add_descriptor(self, name : str, elements : List[str]):
+        """Adds new descriptor to the instance
+
+        Parameters
+        ----------
+        name : str
+            Name of the descriptor
+        elements : List[str]
+            Descriptor elements
+        """
+
         self.descriptors.append(DocStringAttribute(name, elements))
 
     def make_descriptor_string(self):
+        """Generates docstring for instance
+
+        Returns
+        -------
+        desc_string : str
+            String generated for descriptor
+        """
+
         desc_string = ''
         tabs = '    ' * self.doc_level
         descriptor_counter = 0
@@ -66,12 +113,34 @@ class GenerationInstance:
 
 
 class ModuleGenerationInstance:
+    """Top level instance class for module
+
+    Attributes
+    ----------
+    original_module_text : str
+        text from module
+    sub_gen_items : GenerationInstance
+        instances parsed from module
+    """
+
 
     def __init__(self, name : str, original_module_text : List[str]):
+        """Initializer for ModuleGenerationInstance
+        """
+
         self.original_module_text = original_module_text
         self.sub_gen_items = []
 
+
     def get_generated(self):
+        """Writes new file with docstrings
+
+        Returns
+        -------
+        out : str
+            The original file text with docstrings
+        """
+
         out = ''
         line_counter = 0
         while line_counter < len(self.original_module_text):
@@ -91,11 +160,23 @@ class ModuleGenerationInstance:
 
 
     def return_match(self, line : str):
+        """Matches line of original text to generation item for inserting docstring
+
+        Parameters
+        ----------
+        line : str
+            line of text
+
+        Returns
+        -------
+        match : List[str, int, str]
+            Collects instance information for writing stage
+        """
+
         match = None
         for item in self.sub_gen_items:
             if len(line.strip().split(' ', 1)) > 1:
                 if line.strip().split(' ', 1)[1].startswith(item.name):
-                    print('Matched!')
                     if item.name == '__init__':
                         match = [item.name, item.doc_level, '']
                     else:
@@ -104,15 +185,41 @@ class ModuleGenerationInstance:
 
 
 class GenerationItem:
+    """Main Docstring generator
+
+    Attributes
+    ----------
+    target : str
+        target file path
+    overwrite : bool
+        toggle for overriding target
+    temp_file : str
+        temp file path
+    module_gen_instance : ModuleGenerationInstance
+        the instance for the target module
+    """
+
 
     def __init__(self, target_file_path : os.PathLike, overwrite : bool):
-        self.target = target_file_path
+        """Initializer for GenerationItem
+        """
+
+        self.target = os.path.abspath(target_file_path)
         self.overwrite = overwrite
-        self.temp_file = os.path.join(os.path.dirname(self.target), '__code2npdoc_temp__')
+        basename = os.path.basename(self.target).split('.')[0]
+        self.temp_file = os.path.join(os.path.dirname(self.target), f'__{basename}_temp__')
         self.module_gen_instance = self.create_module_gen_instance()
 
 
     def create_module_gen_instance(self) -> ModuleGenerationInstance:
+        """Parses file into ModuleGenerationInstance
+
+        Returns
+        -------
+        mod_instance : ModuleGenerationInstance
+            generated instance with subinstances added
+        """
+
         target_fp = open(self.target, 'r')
         contents = target_fp.readlines()
         mod_instance = ModuleGenerationInstance(os.path.basename(self.target), contents)
@@ -159,6 +266,9 @@ class GenerationItem:
 
 
     def generate_np_docs(self) -> None:
+        """Top level driver function for docstring generation
+        """
+
         temp_fp = open(self.temp_file, 'w')
         temp_fp.write(self.module_gen_instance.get_generated())
         temp_fp.close()
@@ -171,18 +281,57 @@ class GenerationItem:
 
 
 class DocGenerator:
+    """Main Driver object
+
+    Attributes
+    ----------
+    target : str
+        target location
+    ignore_list : List[str]
+        list of files to ignore
+    """
+
 
     def __init__(self, target : os.PathLike, ignore_list : List[str]):
+        """Initializer for DocGenerator
+        """
+
         self.target = target
         self.ignore_list = ignore_list
 
+
     def generate_docs(self, overwrite):
+        """Top level driver function
+
+        Parameters
+        ----------
+        overwrite : bool
+            toggle for overwriting files
+        """
+
         for item in generate_generation_item_list(self.target, self.ignore_list, overwrite):
             item.generate_np_docs()
 
 
 
 def generate_generation_item_list(target: os.PathLike, ignore_list: List[str], overwrite : bool) -> List[GenerationItem]:
+    """Spawns generation item list given package path
+
+    Parameters
+    ----------
+    target: os.PathLike
+        package path
+    ignore_list: List[str]
+        files to ignore
+    overwrite : bool
+        overwrite files toggle
+
+    Returns
+    -------
+    generation_item_list : List[GenerationItem]
+        list of GenerationItems, with each representing one module
+    """
+
     generation_item_list = []
 
     if os.path.isfile(target):
@@ -197,12 +346,41 @@ def generate_generation_item_list(target: os.PathLike, ignore_list: List[str], o
 
 
 def err_exit(message: str, code: int) -> None:
+    """Exits with error code
+
+    Parameters
+    ----------
+    message: str
+        Error message
+    code: int
+        Error code
+    """
+
 
     print(f'ERROR - {message}')
     exit(code)
 
 
 def check_input_output_valid(target: os.PathLike, ignore_list: List[str]) -> (bool, int, str):
+    """Checks if inputs are valid
+
+    Parameters
+    ----------
+    target: os.PathLike
+        target file path
+    ignore_list: List[str]
+        list of files to ignore
+
+    Returns
+    -------
+    valid : bool
+        Are the inputs and outputs valid
+    err_code : int
+        Error code to display if applicable
+    err_message : str
+        Error message
+    """
+
 
     valid = False
     err_code = -1
@@ -224,6 +402,9 @@ def check_input_output_valid(target: os.PathLike, ignore_list: List[str]) -> (bo
 
 
 def print_version_info() -> None:
+    """Prints version and copyright info
+    """
+
     print(f'npdoc2md v{__version__}\n')
     print(f'Copyright (c) {__copyright__}')
     print(f'Author: {__author__}')
@@ -232,6 +413,16 @@ def print_version_info() -> None:
 
 
 def parse_args():
+    """Parses arguments
+
+    Returns
+    -------
+    generator : DocGenerator
+        Main Generator object
+    not args['createtemp'] : bool
+        Toggle for overwriting files
+    """
+
     parser = argparse.ArgumentParser(description='A utility for auto-creating base numpy style docstrings for an entire python project.')
     parser.add_argument('-v', '--version', action='store_true', help='Add this flag for displaying version information.')
     parser.add_argument('-i', '--input', required= not ('-v' in sys.argv or '--version' in sys.argv), help='Path to target python project or file')
@@ -257,6 +448,9 @@ def parse_args():
     return generator, not args['createtemp']
 
 def main():
+    """Main function
+    """
+
     generator, overwrite = parse_args()
     generator.generate_docs(overwrite)
 
