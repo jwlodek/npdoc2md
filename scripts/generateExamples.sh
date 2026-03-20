9c36a5ce1550e2422098236e3ee1ecfc07309b57
Jakub Wlodek <jwlodek.dev@gmail.com>
2026-03-19 22:04:47 -0400

    Remove scripts directory

--- scripts/generateExamples.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ..
-./npdoc2md.py -i . -o example/ -s setup.py __init__.py test_parsing.py
--- scripts/publishPyPi.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-py -m pip install setuptools twine wheel
-py -m pip install --upgrade setuptools twine wheel
-py setup.py sdist bdist_wheel
-twine upload dist/*
-cd scripts
